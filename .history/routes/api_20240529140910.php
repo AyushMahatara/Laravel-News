16215d2de3dd5a079a98da6210ccaba46ca71774
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-05-29 14:21:15 +0545

    updat

--- .history/routes/api_20240529140910.php
+++ .history/routes/api_20240529140910.php
@@ -1,14 +1,8 @@
 <?php
 
-use App\Models\News;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::prefix('v1')->group(function(){
-
-    Route::apiResource('/news', ::class);
-});
--- .history/routes/api_20240529141915.php
+++ .history/routes/api_20240529141915.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\News;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,7 +7,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function(){
 
-    Route::apiResource('/news', ::class);
-});
+Route::apiResource('')
